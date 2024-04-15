5333c95232136477ed9eebac6837d59d005099ff
nikarcosta <mrc32467098@outlook.com>
2024-04-15 08:26:19 -0300

    fix: change messages schema; rename users schema to participants schema

--- src/schemas/authSchema.js
+++
@@ -1,5 +0,0 @@
-import joi from "joi";
-
-export const userSchema = joi.object({
-  username: joi.string().required().min(3),
-});
--- src/schemas/messagesSchema.js
+++ src/schemas/messagesSchema.js
@@ -1,9 +1,7 @@
 import joi from "joi";
 
 export const messageSchema = joi.object({
-  from: joi.string().trim().min(3).required(),
   to: joi.string().required(),
   text: joi.string().required(),
-  type: joi.string().required(),
-  time: joi.date().timestamp(),
+  type: joi.string().valid("message", "private_message").required(),
 });
